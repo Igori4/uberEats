cc2bdb574e021d7eed62d9dde77ddf89bb79ac81
Igori4 <igorpilyak@gmail.com>
2019-05-24 01:46:31 +0300

    Solution

---
+++ cards-info.js
@@ -0,0 +1,69 @@
+'use strict';
+
+let cards = [
+    {
+        'id':'card-1',
+        'name':'Макдоналдс',
+        'category':'₴₴ • Бургеры',
+        'delivery-time':'25 - 35 Min',
+        'image': '/images/image-cards/image-1.png'
+    },
+    {
+        'id':'card-2',
+        'name':'WOKWE!',
+        'category':'₴₴ • Китайська • Азіатська ',
+        'delivery-time':'45 - 50 Min',
+        'image': '/images/image-cards/image-2.png'
+    },
+    {
+        'id':'card-3',
+        'name':'YIZHA',
+        'category':'₴₴ • Американська • Бургеры • Вулична їжа',
+        'delivery-time':'35 - 45 Min',
+        'image': '/images/image-cards/image-3.png'
+    },
+    {
+        'id':'card-4',
+        'name':'Musafir(вул.Саксаганського)',
+        'category':'₴₴ • Близькосхідна',
+        'delivery-time':'30 - 40 Min',
+        'image': '/images/image-cards/image-4.png'
+    },
+    {
+        'id':'card-5',
+        'name':'WOKWEI',
+        'category':'₴₴ • Китайська • Азіатська',
+        'delivery-time':'40 - 50 Min',
+        'image': '/images/image-cards/image-5.png'
+    },
+    {
+        'id':'card-6',
+        'name':'Milk Bar',
+        'category':'₴₴ • Десерти • Європейська',
+        'delivery-time':'25 - 35 Min',
+        'image': '/images/image-cards/image-6.png'
+    },
+    {
+        'id':'card-7',
+        'name':'Mymoza Brooklyn Pizza',
+        'category':'₴₴ • Італійська • Піца • Американська',
+        'delivery-time':'40 - 55 Min',
+        'image': '/images/image-cards/image-7.png'
+    },
+    {
+        'id':'card-8',
+        'name':'To Be',
+        'category':'₴₴ • Японська • Азіатська • Суші',
+        'delivery-time':'45 - 50 Min',
+        'image': '/images/image-cards/image-8.png'
+    },
+    {
+        'id':'card-9',
+        'name':'Druzi Cafe(вул.Прорізна)',
+        'category':'₴₴ • Американська • Сучасна Європейська ',
+        'delivery-time':'45 - 50 Min',
+        'image': '/images/image-cards/image-9.png'
+    },
+
+]
+export default cards;
---
+++ main.js
@@ -0,0 +1,22 @@
+import cards from './cards-info.js'
+
+function addRestourants(element,cards) {
+   cards.map(el => {
+    element.innerHTML +=`
+    <div class="container__card">
+      <a href="#!" id ="${el['id']}">
+      <img  class="card__img" src='${el['image']}'>
+      </a>
+      <div class="card__description">
+        <h3 class="description__text">${el['name']}</h3>
+        <p class="description__category">${el['category']}</p>
+        <span class="description__delivery-time">${el['delivery-time']}</span>
+      </div>
+    </div>  
+   `
+   }).join('')
+  
+}
+
+addRestourants(document.querySelector('[data-element="restourant-cards"]'), cards);
+
